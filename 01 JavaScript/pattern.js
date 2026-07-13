b653be602658f7cf094791b642badd19056fa52f
Gyanendra <pgyanendra333@gmail.com>
2026-07-13 15:37:23 +0530

    implement pattern

---
+++ 01 JavaScript/pattern.js
@@ -0,0 +1,39 @@
+
+
+// let n = 4;
+
+// for (let i = 1; i <= n; i++) {
+//     let row = "";
+//     for (let j = 1; j <= n; j++) {
+//         row += "* ";
+//     }
+//     console.log(row);
+// }
+
+// console.log("Pattern Break");
+
+// let num = 5;
+// function pattern(num) {
+//     for (let i = 1; i <= num; i++) {
+//         let str = "";
+//         for (let j = 1; j <= i; j++) {
+//             str += "* ";
+//         }
+//         console.log(str);
+//     }
+// }
+// pattern(num);
+
+
+
+let n = 6;
+function pattern(n) {
+    for (let i = 1; i <= n; i++) {
+        let row = "";
+        for (let j = 1; j <= i; j++) {
+            row += "$ ";
+        }
+        console.log(row);
+    }
+}
+pattern(n);
