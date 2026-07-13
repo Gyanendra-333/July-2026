67c45f79b7d861d1e71332222ba9b51993589f54
Gyanendra <pgyanendra333@gmail.com>
2026-07-13 16:11:11 +0530

    clean up pattern.js

--- 01 JavaScript/pattern.js
+++ 01 JavaScript/pattern.js
@@ -26,14 +26,44 @@
 
 
 
-let n = 6;
-function pattern(n) {
-    for (let i = 1; i <= n; i++) {
-        let row = "";
-        for (let j = 1; j <= i; j++) {
-            row += "$ ";
-        }
-        console.log(row);
+// let n = 6;
+// function pattern(n) {
+//     for (let i = 1; i <= n; i++) {
+//         let row = "";
+//         for (let j = 1; j <= i; j++) {
+//             row += "$ ";
+//         }
+//         console.log(row);
+//     }
+// }
+// pattern(n);
+
+
+// let n = 5;
+
+// for (let i = n; i >= 1; i--) {
+//     let row = "";
+//     for (let j = 1; j <= i; j++) {
+//         row += "*";
+//     }
+//     console.log(row);
+// }
+
+
+let n = 5;
+
+for (let i = 1; i <= n; i++) {
+    let row = "";
+
+    // Spaces
+    for (let j = 1; j <= n - i; j++) {
+        row += " ";
     }
+
+    // Stars
+    for (let k = 1; k <= 2 * i - 1; k++) {
+        row += "*";
+    }
+
+    console.log(row);
 }
-pattern(n);
